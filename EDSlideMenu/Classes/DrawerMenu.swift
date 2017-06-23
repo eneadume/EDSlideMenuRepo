1b5b840af96d0c25ea727432612b71f40cc1abc0
Enea Dume <eneadume@gmail.com>
2017-06-23 08:41:01 -0700

    fixed problems

---
+++ EDSlideMenu/Classes/DrawerMenu.swift
@@ -0,0 +1,21 @@
+//
+//  DrawerMenu.swift
+//  Pods
+//
+//  Created by Enea Dume on 6/23/17.
+//
+//
+
+import UIKit
+
+class DrawerMenu: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ EDSlideMenu/Classes/extension.swift
@@ -0,0 +1,9 @@
+//
+//  extension.swift
+//  Pods
+//
+//  Created by Enea Dume on 6/23/17.
+//
+//
+
+import Foundation
